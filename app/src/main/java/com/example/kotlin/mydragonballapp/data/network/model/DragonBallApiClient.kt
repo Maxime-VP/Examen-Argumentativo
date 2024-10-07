a1d5b2f350ef4510c3d1bdbf6a9dd5389901d263
Maxime VP <a01710550@tec.mx>
2024-10-07 14:40:09 -0600

    Buscar personaje por nombre

--- app/src/main/java/com/example/kotlin/mydragonballapp/data/network/model/DragonBallApiClient.kt
+++ app/src/main/java/com/example/kotlin/mydragonballapp/data/network/model/DragonBallApiClient.kt
@@ -2,13 +2,12 @@ package com.example.kotlin.mydragonballapp.data.network
 
 import android.util.Log
 import com.example.kotlin.mydragonballapp.data.network.model.CharacterBase
+import com.example.kotlin.mydragonballapp.data.network.model.CharacterListResponse
 import com.example.kotlin.mydragonballapp.data.network.model.CharactersObject
-import retrofit2.Response
 
 class DragonBallApiClient {
     private val api: DragonBallAPIService = NetworkModuleDI()
 
-    // Método para obtener la lista de personajes
     suspend fun getCharacterList(page: Int, limit: Int): CharactersObject? {
         return try {
             val response = api.getCharacterList(page, limit)
@@ -19,24 +18,38 @@ class DragonBallApiClient {
                 null
             }
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.e("API_EXCEPTION", "Exception occurred: ${e.message}")
             null
         }
     }
 
-
-    // Método para obtener la información de un personaje específico por ID
-    suspend fun getCharacterInfo(characterId: Int): CharacterBase? {
+    suspend fun searchCharactersByName(name: String): CharacterListResponse? {
         return try {
-            val response: Response<CharacterBase> = api.getCharacterInfo(characterId)
+            val response = api.getCharactersByName(name)
             if (response.isSuccessful) {
                 response.body()
             } else {
-                Log.e("DragonBallApiClient", "Error: ${response.code()} - ${response.message()}")
+                Log.e("API_ERROR", "API call failed: ${response.code()} - ${response.message()}")
                 null
             }
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.e("API_EXCEPTION", "Exception occurred: ${e.message}")
+            null
+        }
+    }
+
+    // Método para obtener información de un personaje específico por ID
+    suspend fun getCharacterInfo(characterId: Int): CharacterBase? {
+        return try {
+            val response = api.getCharacterInfo(characterId)
+            if (response.isSuccessful) {
+                response.body()
+            } else {
+                Log.e("API_ERROR", "API call failed: ${response.code()} - ${response.message()}")
+                null
+            }
+        } catch (e: Exception) {
+            Log.e("API_EXCEPTION", "Exception occurred: ${e.message}")
             null
         }
     }
--- app/src/main/java/com/example/kotlin/mydragonballapp/framework/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/mydragonballapp/framework/views/activities/MainActivity.kt
@@ -8,24 +8,22 @@ import com.example.kotlin.mydragonballapp.R
 import com.example.kotlin.mydragonballapp.databinding.ActivityMainBinding
 import com.example.kotlin.mydragonballapp.framework.viewmodel.MainViewModel
 import com.example.kotlin.mydragonballapp.framework.views.fragments.CharacterListFragment
+import com.example.kotlin.mydragonballapp.framework.views.fragments.SearchFragment
 import com.example.kotlin.mydragonballapp.utils.Constants
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
-    private lateinit var currentFragment: Fragment
+    private var currentFragment: Fragment? = null
     private var currentMenuOption: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initializeBinding()
-        initializeObservers()
         initializeListeners()
-
-        // Mostrar el fragmento de lista de personajes al iniciar la actividad principal
-        exchangeCurrentFragment(CharacterListFragment(), Constants.MENU_CHARACTER_LIST)
+        showInitialFragment()
     }
 
     private fun initializeBinding() {
@@ -33,30 +31,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    private fun initializeObservers() {
-        // Aquí se pueden añadir observadores si hay algún LiveData en el ViewModel que queremos monitorear.
+    private fun showInitialFragment() {
+        // Inicializamos currentFragment con el fragmento de lista de personajes
+        val characterListFragment = CharacterListFragment()
+        currentFragment = characterListFragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_content_main, characterListFragment)
+            .commit()
+
+        currentMenuOption = Constants.MENU_CHARACTER_LIST
     }
 
     private fun initializeListeners() {
-        // Listener para la opción de la lista de personajes
-        binding.appBarMain.llPokedex.setOnClickListener {
+        binding.appBarMain.llPersonajes.setOnClickListener {
             selectMenuOption(Constants.MENU_CHARACTER_LIST)
         }
 
-        // Listener para la opción de búsqueda
         binding.appBarMain.llSearch.setOnClickListener {
             selectMenuOption(Constants.MENU_SEARCH)
         }
     }
 
     private fun exchangeCurrentFragment(newFragment: Fragment, newMenuOption: String) {
-        currentFragment = newFragment
+        // Validar si currentFragment es null para evitar crasheos
+        if (currentFragment == null || newFragment::class != currentFragment!!::class) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_main, newFragment)
+                .commit()
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment_content_main, currentFragment)
-            .commit()
-
-        currentMenuOption = newMenuOption
+            currentFragment = newFragment
+            currentMenuOption = newMenuOption
+        }
     }
 
     private fun selectMenuOption(menuOption: String) {
@@ -66,6 +71,7 @@ class MainActivity : AppCompatActivity() {
 
         when (menuOption) {
             Constants.MENU_CHARACTER_LIST -> exchangeCurrentFragment(CharacterListFragment(), Constants.MENU_CHARACTER_LIST)
+            Constants.MENU_SEARCH -> exchangeCurrentFragment(SearchFragment(), Constants.MENU_SEARCH)
         }
     }
 }
